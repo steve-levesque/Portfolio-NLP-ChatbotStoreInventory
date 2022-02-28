7f0f2a0debaa9ad0ef5eac760c8f7bf31222785d
Steve Lévesque <42849270+steve-levesque@users.noreply.github.com>
2022-02-28 11:54:25 -0500

    [Fix] Tensorflow reset_default_graph()
    - Accidental remove fixed for tf.compat.v1.reset_default_graph() in the model.

--- models/tensorflow_model.py
+++ models/tensorflow_model.py
@@ -1,7 +1,10 @@
+import tensorflow as tf
 import tflearn
 
 
 def tf_model_network(train_X, train_y):
+    tf.compat.v1.reset_default_graph()
+
     net = tflearn.input_data(shape=[None, len(train_X[0])])
     net = tflearn.fully_connected(net, 8)
     net = tflearn.fully_connected(net, 8)
